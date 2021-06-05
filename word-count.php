47e2a7461c5f21fd88ab660856e6d2b829202075
Mahadi001 <sadat.himel@gmail.com>
2021-06-05 19:20:31 +0600

    update

--- word-count.php
+++ word-count.php
@@ -32,7 +32,30 @@ function wordcount_count_words($content){
     $stripped_content = strip_tags($content);
     $wordn = str_word_count($stripped_content);
     $label = __('Total Number of Words','word-count');
-    $content .= sprintf('<h2>%s: %s</h2>',$label,$wordn);
+
+    $label = apply_filters("wordcount_heading",$label);
+    $tag = apply_filters("wordcount_tag",'h2');
+    $content .= sprintf('<%s>%s: %s</%s>',$tag,$label,$wordn,$tag);
     return $content;
 }
 add_filter('the_content','wordcount_count_words');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
