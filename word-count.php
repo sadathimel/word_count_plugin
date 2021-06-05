f9b348259aca39e046ce5ea16f19936b63be976f
Mahadi001 <sadat.himel@gmail.com>
2021-06-05 16:20:10 +0600

    this is my first word count plugin

---
+++ word-count.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Word Count
+Plugin URI: 
+Description: count words from any wordpress site
+Version: 1.0
+Author: sadat himel
+Author URI: http://github.com/sadathimel/
+License: GPLv2 or later
+Text Domain: word-count
+Domain Path: /languages
+*/
+
+
+
+function wordcount_activition_hook(){
+
+}
+register_activation_hook(__FILE__,"wordcount_activition_hook");
+
+function wordcount_deactivition_hook(){
+
+}
+register_deactivation_hook(__FILE__,"wordcount_deactivition_hook");
+
+
