9c8dc76cd6cba8632d85589f397da664cd360e6f
Mahadi001 <sadat.himel@gmail.com>
2021-06-05 18:08:53 +0600

    update

--- word-count.php
+++ word-count.php
@@ -27,3 +27,12 @@ function wordcount_load_textdomain(){
     load_plugin_textdomain('word-count',false,dirname(__FILE__)."/languages");
 }
 add_action( "plugins_loaded", 'wordcount_load_textdomain');
+
+function wordcount_count_words($content){
+    $stripped_content = strip_tags($content);
+    $wordn = str_word_count($stripped_content);
+    $label = __('Total Number of Words','word-count');
+    $content .= sprintf('<h2>%s: %s</h2>',$label,$wordn);
+    return $content;
+}
+add_filter('the_content','wordcount_count_words');
